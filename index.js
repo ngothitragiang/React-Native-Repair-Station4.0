1421b7b0959963f4292129c80b2bb6914c36eae4
Nam-Dang-Dev <dangphuongnamqt@gmail.com>
2020-03-12 18:16:47 +0700

    renamed src

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 import {Navigation} from 'react-native-navigation';
-import SplashScreen from './src/screens/SplashScreen';
 
-import startApp from './fixer/navigation/bottomTab';
-import {setRoot} from './fixer/navigation/function';
-Navigation.registerComponent('SplashScreen', () => SplashScreen);
+import startApp from './src/navigation/bottomTab';
+import {setRoot} from './src/navigation/function';
 import firebase from 'react-native-firebase';
-import {registerScreens} from './fixer/navigation/registerScreens';
+import {registerScreens} from './src/navigation/registerScreens';
 import {AsyncStorage} from 'react-native';
 registerScreens();
-// firebase.auth().onAuthStateChanged(user => {
-//   if (user) {
-//     startApp();
-//   } else {
-//     setRoot('login');
-//   }
-// });
 
 Navigation.events().registerAppLaunchedListener(async () => {
   const stationData = await AsyncStorage.getItem('tokenDevice');
